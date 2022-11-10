b94b142221dfbebac9aa4ef9d0e73711fd37fb2d
Suyash <paradisestrings99@gmail.com>
2022-11-10 17:04:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+	"mongodb+srv://masuya:cloudD3@cluster0.fv9hp5q.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
+	)
+	.catch((error) => console.log(error.message));
